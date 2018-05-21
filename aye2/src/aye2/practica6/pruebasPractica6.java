36609c5f0e32c5f020b2d7645676f07b2402e065
german.reppin <german.reppin@gmail.com>
2018-05-21 10:07:50 -0300

    Guardado2

---
+++ aye2/src/aye2/practica6/pruebasPractica6.java
@@ -0,0 +1,18 @@
+package aye2.practica6;
+
+public class pruebasPractica6 {
+
+	public static void main(String[] args) {
+		
+		Lamparita lamp1 = new Lamparita();
+		
+		System.out.printIn("La lamparita esta " + lamp1.getEstado().toString());
+        
+		lamp1.encender();
+		
+		System.out.println("La lamparita esta " + lamp1.getEstado());
+		
+		
+	}
+
+}
