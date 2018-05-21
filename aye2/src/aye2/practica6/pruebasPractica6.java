7601727010d6f591cf760d0d77b77519f19345ad
german.reppin <german.reppin@gmail.com>
2018-05-21 11:32:35 -0300

    EjercicioEnteroConfallas

--- aye2/src/aye2/practica6/pruebasPractica6.java
+++ aye2/src/aye2/practica6/pruebasPractica6.java
@@ -13,6 +13,15 @@ public class pruebasPractica6 {
 		System.out.println("La lamparita esta " + lamp1.getEstado());
 		
 		
+		//Ejercicio 6: Prueba de Hora
+		Hora h1 = new Hora(1, 140, 160);
+		Hora h2 = new hora(2, 35, 102);
+		
+		System.out.println("La hora h1 es: " + h1.toString());
+		
+		System.out.println("La suma de h1 y h2 es; " + Hora.sumar(h1, h2)); // es igual que hacer: System.out.println("La suma de h1 y h2 es; " + h1);
+		
+		
 	}
 
 }
