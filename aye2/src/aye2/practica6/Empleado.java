4b411525498b4520fa045913eda1fcf7c69b7524
german.reppin <german.reppin@gmail.com>
2018-05-21 11:45:49 -0300

    Completo

--- aye2/src/aye2/practica6/Empleado.java
+++ aye2/src/aye2/practica6/Empleado.java
@@ -12,25 +12,29 @@ public class Empleado {
 	
 	public Empleado(int n, String nombre) {
 		numero = n;
-		
+		this.nombre = nombre;
 		
 	}
+	
 	public int getNumero() {
 		return numero;
 	}
+	
 	public void setNumero(int numero) {
 		this.numero = numero;
 	}
+	
 	public string getNombre() {
 		return nombre;
 	}
+	
 	public void setNombre(string nombre) {
 		this.nombre = nombre;
 	}
 	
-	
-	
-	
-	
+	public void verdatos() {
+		System.out.println("Empleado nro " + numero + ":  " + nombre);
+	}
+		
 
 }
